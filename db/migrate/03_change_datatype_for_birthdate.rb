06bf654bb3fc81f80fd6de8126ab63c88ba0a973
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-16 13:21:04 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,7 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
 def change
-  add_column :students,:grade, :integer
-  add_column :students,:birthdate, :string
+  change_column :students,:grade, :integer
+  change_column :students,:birthdate, :string
   
 end
 end
