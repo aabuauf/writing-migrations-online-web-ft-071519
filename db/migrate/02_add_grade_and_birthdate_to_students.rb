7892357ef14d404724914d2bbb98a9a343038afe
Ahmed Abououf <aabuauf@yahoo.com>
2019-08-16 13:17:29 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,5 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
 def change
-  create_table :artists do |t|
-      t.string :name
-  end
+  add_column 
 end
 end
